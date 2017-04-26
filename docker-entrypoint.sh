220090d38a73033b9ae8843e14b53dc75bf084c4
Ville Väänänen <ville.vaananen@lucify.com>
2017-04-26 14:38:34 +0300

    Include a docker-compose.yml file
    Also improve the entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,26 +1,18 @@
 #!/bin/bash
 
-if [ "$1" = 'npm'  ] || [ "$1" = '/bin/bash'  ]; then
+if [ -n "$DNS_HOSTNAME" ]; then
 
-  if [[ -z "$DEBUG" ]] ; then
-    exec 3>&1 &>/dev/null
-  else
-    exec 3>&1
-  fi
-
-
-  dns_ip=$(getent hosts ${DNS_HOSTNAME:-bind.local} | awk '{ print $1 }')
+  dns_ip=$(getent hosts ${DNS_HOSTNAME} | awk '{ print $1 }')
   if [ "$dns_ip" = '' ]; then
-    echo "Local dns_ip not available" >&3
+    echo "Local dns_ip not available"
   else
     until echo -e "nameserver $dns_ip\nnameserver 127.0.0.11" > /etc/resolv.conf; do
-      echo "Failed to write to /etc/resolv.conf, sleeping" >&3
+      echo "Failed to write to /etc/resolv.conf, sleeping"
       sleep 2
     done
-    cat /etc/resolv.conf >&3
+    cat /etc/resolv.conf
   fi
 
 fi
-
 exec "$@"
 
